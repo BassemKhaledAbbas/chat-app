6cc12e71dbfe85ccde3b67719128912abad52ac5
ABBAS Bassem OBS/CMI <bassem.abbas@orange.com>
2020-11-19 12:22:32 +0200

    add connect disconnect event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3300
 let app = express();
+let server = http.createServer(app)
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('new user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    })
+});
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
