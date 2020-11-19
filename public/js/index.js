e31fb63630ac7ea2fdb4be3d2666f1b3bf127f9c
ABBAS Bassem OBS/CMI <bassem.abbas@orange.com>
2020-11-19 17:40:20 +0200

    listen to events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('connected to the server');
+
+    socket.emit('createMessage', {
+        from: 'Andy',
+        text: 'test message'
+
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('disconnected');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('new message', message);
+});
